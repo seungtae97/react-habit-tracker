96ed7f638ab57a562cdabfbeecad264df8bf3aaf
seungtae97 <fo_ma@naver.com>
2021-08-24 17:10:25 +0900

    Test

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ class Navbar extends PureComponent {
     return (
       <div className="navbar">
         <i className="navbar-logo fas fa-leaf"></i>
-        <span>Habit Tracker</span>
+        <span>aHabit Tracker</span>
         <span className="navbar-count">{this.props.totalCount}</span>
       </div>
     );
